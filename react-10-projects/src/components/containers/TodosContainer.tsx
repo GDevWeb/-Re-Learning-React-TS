12eba218fa0faaa3b92c85145fcd6849c9b5e91b
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-07-07 21:24:32 +0200

    state up vers todosContainer en cours

--- react-10-projects/src/components/containers/TodosContainer.tsx
+++ react-10-projects/src/components/containers/TodosContainer.tsx
@@ -1,16 +1,62 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import useTodos from "../../hooks/useTodos";
+import Filter from "../filter/Filter";
 import Form from "../formComponents/Form";
 import ToDoList from "../todos/Todolist";
 
 export default function TodosContainer() {
   const { todosState, addTask, handleCheck, handleDelete } = useTodos();
 
+  // 1.***State*** :
+  const [inputState, setInputState] = useState<string>("Hello");
+  const [error, setError] = useState<string | null>(null);
   const [filter, setFilter] = useState<string>("Toutes");
 
   useEffect(() => {
     document.title = "To do list en TS";
   }, []);
+
+  // Clear error message after 20 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setError(null);
+    }, 20000);
+    return () => clearTimeout(timer);
+  }, [error]);
+
+  // 2. ***Behavior***
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!error && inputState.trim() !== "" && inputState.length > 3) {
+      addTask(inputState);
+      setInputState("");
+    } else {
+      setError(
+        "Impossible de soumettre le formulaire dû à une erreur de validation"
+      );
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    try {
+      if (value.trim() === "" || typeof value !== "string") {
+        throw new Error("Veuillez saisir une chaîne de caractères non vide !");
+      }
+
+      setError(null);
+      setInputState(value);
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+
+  // Filter :
+  const handleFilter = (e: MouseEvent<HTMLButtonElement>) => {
+    const value: string = e.currentTarget.value;
+    setFilter(value);
+  };
 
   const getFilteredTodos = () => {
     if (filter === "Complété") {
@@ -22,14 +68,21 @@ export default function TodosContainer() {
     }
   };
 
+  // 3. ***Render*** :
   return (
-    <div className="max-w-md mx-auto mt-10 p-4 bg-white rounded shadow-lg">
-      <Form />
+    <div className="max-w-max mx-auto mt-10 p-4 bg-white rounded shadow-lg">
+      <Form
+        handleSubmit={handleSubmit}
+        inputState={inputState}
+        handleInputChange={handleInputChange}
+        error={error}
+      />
       <ToDoList
         todos={getFilteredTodos()}
         handleDelete={handleDelete}
         handleCheck={handleCheck}
       />
+      <Filter handleFilter={handleFilter} />
     </div>
   );
 }
--- react-10-projects/src/components/formComponents/Form.tsx
+++ react-10-projects/src/components/formComponents/Form.tsx
@@ -1,78 +1,28 @@
-import { ChangeEvent, FormEvent, MouseEvent, useEffect, useState } from "react";
-import useTodos from "../../hooks/useTodos"; // Adjust the path accordingly
-import Filter from "../filter/Filter";
+import { ChangeEvent, FormEvent } from "react";
 import ButtonForm from "../todos/Button";
 import Input from "../todos/Input";
 import Label from "../todos/Label";
 
-export default function Form() {
-  // 1. ***State***
-  const [inputState, setInputState] = useState<string>("");
-  const [error, setError] = useState<string | null>(null);
-  const [filter, setFilter] = useState<string>("Toutes");
+interface FormProps {
+  handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
+  handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  inputState: string;
+  error: string | null;
+}
 
-  const { todosState, addTask, handleCheck, handleDelete } = useTodos();
-
-  // Clear error message after 20 seconds
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setError(null);
-    }, 20000);
-    return () => clearTimeout(timer);
-  }, [error]);
-
-  // 2. ***Behavior***
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!error && inputState.trim() !== "" && inputState.length > 3) {
-      addTask(inputState);
-      setInputState("");
-    } else {
-      setError(
-        "Impossible de soumettre le formulaire dû à une erreur de validation"
-      );
-    }
-  };
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-
-    try {
-      if (value.trim() === "") {
-        throw new Error("Veuillez saisir une chaîne de caractères non vide !");
-      }
-
-      if (typeof value !== "string") {
-        throw new Error("Veuillez saisir une chaîne de caractères");
-      }
-
-      setError(null);
-      setInputState(value);
-    } catch (error: any) {
-      setError(error.message);
-    }
-  };
-
-  const handleFilter = (e: MouseEvent<HTMLButtonElement>) => {
-    const value: string = e.currentTarget.value;
-    setFilter(value);
-  };
-
-  const getFilteredTodos = () => {
-    if (filter === "Complété") {
-      return todosState.filter((todo) => todo.completed);
-    } else if (filter === "Non complété") {
-      return todosState.filter((todo) => !todo.completed);
-    } else {
-      return todosState;
-    }
-  };
-
-  // 3. ***Render***
+export default function Form({
+  handleSubmit,
+  inputState,
+  handleInputChange,
+  error,
+}: FormProps) {
   return (
     <div className="max-w-md mx-auto mt-10 p-4 bg-white rounded shadow-lg">
-      <h1 className="text-4xl text-center mb-4">Ma to do liste </h1>
-      <form onSubmit={handleSubmit} className="mb-4">
+      <h1 className="text-4xl text-center mb-4">Ma to do liste</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto p-4 bg-white rounded shadow-lg mb-4"
+      >
         <div className="formGroup mb-2">
           <Label name="inputName" title="Saisir nom de la tâche" />
           <Input
@@ -84,14 +34,6 @@ export default function Form() {
         </div>
         <ButtonForm type="submit" textButton="Ajouter" />
       </form>
-
-      {/* <Todolist
-        todos={getFilteredTodos()}
-        handleDelete={handleDelete}
-        handleCheck={handleCheck}
-      /> */}
-
-      <Filter handleFilter={handleFilter} />
     </div>
   );
 }
